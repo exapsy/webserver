41e7333eefa5b4c6c3f524e802259140ea44d417
exapsy <apostolis.anastasiou.alpha@gmail.com>
2020-04-09 14:36:12 +0300

    Add custom port support

--- README.md
+++ README.md
@@ -1,3 +1,21 @@
 # Custom webserver
 
 This is a project to understand how webservers work by making my very own one.
+
+## USAGE
+
+```bash
+# Make a `build` directory inside repository
+mkdir build
+cd build
+
+# Build makefiles via cmake inside build directory
+cmake .. .
+
+# Compile project
+make
+
+# Run webserver
+./webserver # Default port 3000
+./webserver 3005 # Runs webserver at port 3005
+```
--- main.c
+++ main.c
@@ -1,9 +1,17 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 #include "./cmd/server.h"
 
-int main() {
-  Error *err = start_webserver(3000);
+int main(int argc, char **argv) {
+  int port;
+  if (argc == 1) {
+    port = 3000;
+  } else if (argc >= 2) {
+    string port_str = argv[1];
+    port = atoi(port_str);
+  }
+  Error *err = start_webserver(port);
   if (err != NULL) {
     err_print_fatal(err);
   }
